fa7f7d12412ee26d800d1374bc388a003ba0448e
Lorenzo Testa <testa@intre.it>
2019-10-26 12:02:34 +0200

    first commit, 0%

---
+++ src/test/java/xyz/testalorenzo/MainTest.java
@@ -0,0 +1,18 @@
+package xyz.testalorenzo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+//    @Test
+//    void main() {
+//
+//    }
+
+    @Test
+    void play() {
+        Main.play(4);
+    }
+}
