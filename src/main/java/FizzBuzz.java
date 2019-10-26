48725a8ee8711caa053b6b87e741b8a9ea6e50ce
Lorenzo Testa <testa@intre.it>
2019-10-26 18:42:21 +0200

    coverage 100% on FizzBuzz

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class FizzBuzz {
+
+    private int FIZZ;
+    private int BUZZ;
+
+    FizzBuzz(int fizz, int buzz) {
+        this.FIZZ = fizz;
+        this.BUZZ = buzz;
+    }
+
+    List<String> play(int num) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= num; i++)
+        {
+            if (((i % FIZZ) == 0) && ((i % BUZZ) == 0)) // Is it a multiple of fizz & buzz?
+                result.add("fizzbuzz");
+            else if ((i % FIZZ) == 0) // Is it a multiple of fizz?
+                result.add("fizz");
+            else if ((i % BUZZ) == 0) // Is it a multiple of BUZZ?
+                result.add("buzz");
+            else
+                result.add(String.valueOf(i)); // Not a multiple of FIZZ or BUZZ
+        }
+        return result;
+    }
+}
---
+++ src/test/java/TestFizzBuzz.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class TestFizzBuzz {
+
+    @Test
+    public void playTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz(2, 3);
+        fizzBuzz.play(7);
+    }
+
+
+
+}
